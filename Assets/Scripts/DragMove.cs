7e4e5810323d4f639a4e28aac4d9cfe5cbd73587
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-15 08:25:52 +0300

    DragMove script implemented

---
+++ Assets/Scripts/DragMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragMove : MonoBehaviour {
+    
+    private Vector3 touchPosition;
+    private Rigidbody2D rigidbody;
+    private Vector3 direction;
+    private float moveSpeed = 10f;
+    
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            touchPosition.z = 0;
+            direction = touchPosition - transform.position;
+            rigidbody.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+
+            if (touch.Phase == TouchPhase.Ended) {
+                rigidbody.velocity = Vector2.zero;
+            }
+        }
+    }
+}
