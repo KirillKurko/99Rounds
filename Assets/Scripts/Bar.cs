962c3588cc149104365942282aee8edf39415348
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-09 14:16:12 +0300

    Progress bar added

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+using UnityEngine.SceneManagement;
+
+public class Bar : MonoBehaviour {
+
+    public GameObject bar;
+    public int time;
+
+    void Start() {
+        AnimateBar();
+    }
+
+    void Update() {
+        if (bar.transform.localScale.x == 1) {
+            Scene scene = SceneManager.GetActiveScene(); 
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    public void AnimateBar() {
+        LeanTween.scaleX(bar, 1, time);
+    }
+}
