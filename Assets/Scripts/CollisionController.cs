dcf10ea07508e197f10214f57aaa65138b5f2337
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-09 09:22:08 +0300

    scene reload on enemy and player collision addded, enemy collision disabled

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionController : MonoBehaviour {
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag.Equals("player")) {
-            Destroy(collision.gameObject);
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag.Equals("player")) {
+            Destroy(collider.gameObject);
             Destroy(gameObject);
+            Scene scene = SceneManager.GetActiveScene(); 
+            SceneManager.LoadScene(scene.name);
         }
     }
 }
