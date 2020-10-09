9b35c3bbbee5386502a5f45c06af62b1928330b0
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-09 14:27:28 +0300

    Now generating random amount of enemies

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -13,9 +13,16 @@ public class EnemyGenerator : MonoBehaviour {
     void Update() {
         if (Time.time > nextSpawn) {
             nextSpawn = Time.time + spawnRate;
-            randomX = Random.Range(-2.6f, 2.6f);
-            spawnPosition = new Vector3(randomX, transform.position.y, -1);
-            Instantiate(enemy, spawnPosition, Quaternion.identity);
+            int enemiesAmount = Random.Range(1, 5);
+            for (int i = 0; i < enemiesAmount; ++i) {
+                GenerateEnemy();
+            }
         }
+    }
+
+    private void GenerateEnemy() {
+        randomX = Random.Range(-2.6f, 2.6f);
+        spawnPosition = new Vector3(randomX, transform.position.y, -1);
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
     }
 }
